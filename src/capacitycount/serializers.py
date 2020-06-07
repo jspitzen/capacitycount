ee8b2f55d2a224da12dd6f3fa0fb32e2df68c33c
Jorick Spitzen <jorick.spitzen@veneficus.nl>
2020-06-07 21:42:20 +0200

    Add list views for Users and Bookings that don't expose ids and only work when the room_id is known

--- src/capacitycount/serializers.py
+++ src/capacitycount/serializers.py
@@ -2,10 +2,12 @@ from rest_framework import serializers
 
 from .models import Room, User, Booking
 
+
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = '__all__'
+
 
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,4 +18,18 @@ class RoomSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = '__all__'
+
+
+class PrivateUserSerializer(serializers.ModelSerializer):
+    # Don't expose the user id
+    class Meta:
+        model = User
+        fields = ['name']
+
+class PrivateBookingSerializer(serializers.ModelSerializer):
+    # Don't expose user id
+    user = serializers.StringRelatedField()
+    class Meta:
+        model = Booking
         fields = '__all__'
--- src/capacitycount/views.py
+++ src/capacitycount/views.py
@@ -1,15 +1,39 @@
 from rest_framework import mixins, viewsets
+from rest_framework.response import Response
+from rest_framework.exceptions import MethodNotAllowed
 
 from .models import Booking, Room, User
-from .serializers import BookingSerializer, RoomSerializer, UserSerializer
+from .serializers import (
+    BookingSerializer, RoomSerializer, UserSerializer, PrivateUserSerializer,
+    PrivateBookingSerializer,
+)
 
 
 class BookingViewSet(mixins.CreateModelMixin,
                   mixins.RetrieveModelMixin,
+                  mixins.ListModelMixin,
                   viewsets.GenericViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
+    def list(self, request):
+        room_id = request.query_params.get('room')
+        user_id = request.query_params.get('user')
+        if not room_id and not user_id:
+            raise MethodNotAllowed(
+            'GET',
+            detail='GET not allowed without room_id or user_id')
+        if room_id and user_id:
+            # If the user and the room don't match, it will return empty
+            qs = Booking.objects.filter(user_id=user_id, user__room_id=room_id)
+        elif room_id:
+            # All bookings for a room
+            qs = Booking.objects.filter(user__room_id=room_id)
+        elif user_id:
+            # All bookings for a user
+            qs = Booking.objects.filter(user_id=user_id)
+        serializer = PrivateBookingSerializer(qs, many=True)
+        return Response(serializer.data)
 
 class RoomViewSet(mixins.CreateModelMixin,
                   mixins.RetrieveModelMixin,
@@ -20,6 +44,16 @@ class RoomViewSet(mixins.CreateModelMixin,
 
 class UserViewSet(mixins.CreateModelMixin,
                   mixins.RetrieveModelMixin,
+                  mixins.ListModelMixin,
                   viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def list(self, request):
+        room_id = request.query_params.get('room')
+        if not room_id:
+            raise MethodNotAllowed('GET',
+                                   detail='GET not allowed without room_id')
+        queryset = User.objects.filter(room_id=room_id)
+        serializer = PrivateUserSerializer(queryset, many=True)
+        return Response(serializer.data)
